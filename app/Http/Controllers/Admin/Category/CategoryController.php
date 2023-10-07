81b4f56b59bbfd17177e29d2c7c1c4d7ba5681a5
Zabir Emu <zabirraihan570@gmail.com>
2023-10-07 13:31:46 +0600

    category store created

--- app/Http/Controllers/Admin/Category/CategoryController.php
+++ app/Http/Controllers/Admin/Category/CategoryController.php
@@ -5,12 +5,29 @@ namespace App\Http\Controllers\Admin\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use stdClass;
 
 class CategoryController extends Controller
 {
     public function index()
     {
         $data = DB::table('categories')->get();
-        return view('admin.category.category.index', compact('data'));
+        $info = new stdClass();
+        $info->page_title = 'Create Category';
+        $info->all_data = 'All Categories';
+        return view('admin.category.category.index', compact('data', 'info'));
+    }
+
+    public function store(Request $request)
+    {
+        //dd($request->all());
+        $request->validate([
+            'category_name' => 'required|string|max:80|unique:categories,category_name',
+            'category_slug' => 'required|string|max:80|unique:categories,category_slug',
+        ]);
+
+        $expect_columns = json_decode('["_token"]', true);
+        $row = DB::table('categories')->insert($request->except($expect_columns));
+        return redirect()->route('admin.category.index')->with('success', 'Category Successfully Created');
     }
 }
--- resources/views/admin/category/category/index.blade.php
+++ resources/views/admin/category/category/index.blade.php
@@ -11,17 +11,16 @@
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a href="javascript: void(0);">Dashboard</a></li>
-                    <li class="breadcrumb-item"><a href="javascript: void(0);">Create Category</a></li>
+                    <li class="breadcrumb-item"><a href="javascript: void(0);">{{ $info->page_title }}</a></li>
                 </ol>
             </div>
-            <h4 class="page-title">Create Category</h4>
+            <h4 class="page-title">{{ $info->page_title }}</h4>
         </div>
     </div>
     <div class="row">
         <div class="col-lg-5">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title">Create Category</h4>
                     <form action="{{ route('admin.category.store') }}" method="post">
                         @csrf
                         <div class="mb-3">
@@ -36,10 +35,22 @@
                             @enderror
                         </div>
                         <div class="mb-3">
-                           
+                            <label for="category_slug" class="form-label">Category Slug</label>
+                            <input type="text" id="category_name"
+                                class="form-control @error('category_slug')
+                                        is-invalid
+                                        @enderror"
+                                placeholder="Category Slug" name="category_slug">
+                            @error('category_slug')
+                                <span class="text-danger">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+
                             <div class="form-check form-switch">
-                                <input type="checkbox" class="form-check-input d-none" name="status" value="0">
-                                <input type="checkbox" class="form-check-input" name="status" id="customSwitch1"  value="1">
+                                <input type="checkbox" class="form-check-input d-none" name="status" checked value="0">
+                                <input type="checkbox" class="form-check-input" name="status"
+                                    value="1">
                                 <label class="form-check-label" for="">Status</label>
                             </div>
                         </div>
@@ -53,7 +64,7 @@
         <div class="col-lg-7">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="header-title">Category</h4>
+                    <h4 class="header-title">{{ $info->all_data }}</h4>
                     <table id="basic-datatable" class="table dt-responsive nowrap w-100">
                         <thead>
                             <tr>
@@ -65,8 +76,9 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($data as $item)
+                            @foreach ($data as $key=> $item)
                                 <tr>
+                                    <td>{{$key+1}}</td>
                                     <td>{{ $item->category_name }}</td>
                                     <td>{{ $item->category_slug }}</td>
                                     <td>
@@ -77,7 +89,7 @@
                                         @endif
                                     </td>
                                     <td>
-                                        <a href="" class="btn btn-info waves-effect waves-light">Submit</a>
+                                        <a href="" class="edit"><i class="material-symbols-outlined">edit</i></a>
                                         <span class="delete"><i class="material-symbols-outlined">delete</i></span>
                                     </td>
                                 </tr>
